ab66b68881dc8e0f0da685c5d92e2b73416ffd28
Nerdary <1196116694@qq.com>
2018-12-17 21:18:12 +0800

    fix strcat bug

--- decode.m
+++ decode.m
@@ -11,14 +11,18 @@ function [content] = decode(huffman_result, huffman_code, original_chars)
     Length = length(huffman_code);
     Count = length(huffman_result);
     for i=1:Length
+        debug = 0;
         char = huffman_code(i);         % read next number
         token = strcat(token, char);    % strcat
         for j=1:Count
             result = strcmp(token, huffman_result(j));
             if(result==1)
-                content = strcat(content, original_chars(j));
+                debug = 1;
+                content = [content, original_chars(j)];
                 token = '';             % clear token
             end
+        end
+        if(debug==0)
         end
     end
         
